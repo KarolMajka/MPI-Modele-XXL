7e4e051b37c4ed000bdb0ad72b167533a88e6030
Karol Majka <karol.x.majka@gsk.com>
2017-06-02 10:51:18 +0200

    add barrier at begging

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv) {
     int size,rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     Agent *agent = new Agent(size, rank, S);
     agent->agentLoop();
 
